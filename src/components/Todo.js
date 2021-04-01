356fc1ac44e6fef7824ff848623c315eab2b2eca
Andrew <andrew.z.hale1@gmail.com>
2021-04-01 10:18:36 -0700

    ternary operator

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -10,30 +10,36 @@ function Todo({ todos, completeTodo, removeTodo }) {
     value: "",
   });
 
-  if (todos) {
-    return todos.map((todo, index) => (
-      <div
-        className={todo.isComplete ? "todo-row complete" : "todo-row"}
-        key={index}
-      >
-        <div key={todo.id} onClick={() => completeTodo(todo.id)}>
-          {todo.text}
-        </div>
-        <div className="icons">
-          <VscChromeClose
-            onClick={() => removeTodo(todo.id)}
-            className="delete"
-          />
-          <HiPencil
-            onClick={() => setEdit({ id: todo.id, value: todo.text })}
-            className="edit"
-          />
-        </div>
-      </div>
-    ));
-  } else {
-    return <div></div>;
-  }
+  return (
+    <div>
+      {todos !== undefined ? (
+        <>
+          {todos.map((todo, index) => {
+            <div
+              className={todo.isComplete ? "todo-row complete" : "todo-row"}
+              key={index}
+            >
+              <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+                {todo.text}
+              </div>
+              <div className="icons">
+                <VscChromeClose
+                  onClick={() => removeTodo(todo.id)}
+                  className="delete"
+                />
+                <HiPencil
+                  onClick={() => setEdit({ id: todo.id, value: todo.text })}
+                  className="edit"
+                />
+              </div>
+            </div>;
+          })}
+        </>
+      ) : (
+        <h1>todos.length === 0</h1>
+      )}
+    </div>
+  );
 }
 
 export default Todo;
